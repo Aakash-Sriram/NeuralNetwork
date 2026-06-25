9439032c3995b00d06c191853d2eb324eef23b0a
Aakash Sriram <194975502+Aakash-Sriram@users.noreply.github.com>
2026-06-26 00:50:47 +0530

    untangle the spaghetti feedforward func and untangle mess

--- main.cpp
+++ main.cpp
@@ -4,11 +4,10 @@
 #include<numbers>
 #include<cmath>
 struct Node{
-    float* w;
-    float* x;
+    float* weights;
     float output;
     float bias;
-    float res;//value put in sigmod function
+    float z;//value put in sigmod function
 }
 struct Layer{
     Node* nodes;
@@ -20,6 +19,15 @@ struct NeuralNetwork{
 }
 float sigmoid(float x){
     return (1.0f/(1,0f+std::exp(-x)));
+}
+float feedforward(Node& neuron , Layer& prevLayer){
+    float sum = neuron.bias;
+    for(int i=0;i<prevLayer.noOfNodes;i++){
+        sum+=prevLayer.nodes[i].output * neuron.weights[i];
+    }
+    neuron.z=sums
+    neuron.output=sigmoid(sum)
+    return neuron.output;
 }
 std::vector<int> flatten(std::vector<std::vector<int>> input){
     vector<int> res;
@@ -46,33 +54,5 @@ int main(void){
         }
         std::cout << '\n';
     }
-    // seed random numbers to make calculation
-    std::mt19937 rng(1);  // Seed = 1
-    std::uniform_real_distribution<double> dist(0.0, 1.0);
-
-    int hiddenlayers = 2;
-    NeuralNetwork nn;
-    nn.noOfLayers = hiddenlayers + 2; // input layer + hidden layers + output layer
-    nn.layers = new Layer[nn.noOfLayers];
-
-    arr = flatten(X);
-    nn.layers[0].nodes = new Node[8]
-    for(int i=0;i<8;i++){
-        nn.layers[0].nodes[i].x=arr[i];
-        nn.layers[0].nodes[i].w=dist(rng);
-        nn.layers[0].nodes[i].bias=dist(rng);
-    }
-
-    for(int layer=1;layer<nn.noOfLayers;layer++){
-        nn.layers[layer].noOfNodes = 2; // For simplicity each layer has 2 nodes
-        nn.layers[layer].nodes = new Node[nn.layers[layer].noOfNodes];
-        for(int i=0;i<nn.layers[layer].noOfNodes;i++){
-            nn.layers.nodes[i].w= dist(rng);
-            nn.layers.nodes[i].bias=dist(rng);
-            for(int j=0;j<8;j++){
-                temp = (nn.layers.nodes[j].w*nn.layers.nodes[j].x)
-                nn.layers.nodes[i].x
-            }
-        }
-    }
+    
 }
