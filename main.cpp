920e6cdb345070876e75557c79006c0638060171
Aakash Sriram <194975502+Aakash-Sriram@users.noreply.github.com>
2026-06-25 23:52:40 +0530

    definitions

--- main.cpp
+++ main.cpp
@@ -1,5 +1,22 @@
 #include<iostream>
 #include<vector>
+#include<random>
+struct Node{
+    float* w;
+    float* x;
+    float output;
+    float bias;
+    float res;//value put in sigmod function
+}
+struct Layer{
+    Node* nodes;
+    int noOfNodes;
+}
+struct NeuralNetwork{
+    Layer* layers;
+    int noOfLayers;
+}
+void sigmoid();
 int main(void){
     std::cout<<"Neural Network from ground up in C++ yo" << std::endl;
     std::vector<std::vector<int>> X = {
@@ -8,10 +25,15 @@ int main(void){
         {1, 1, 1},
         {0, 0, 1}
     };
+    //Print the input data
     for (auto x : X) {
         for (auto i : x) {
             std::cout << i << " ";
         }
         std::cout << '\n';
     }
+    // seed random numbers to make calculation
+    std::mt19937 rng(1);  // Seed = 1
+    std::uniform_real_distribution<double> dist(0.0, 1.0);
+
 }
